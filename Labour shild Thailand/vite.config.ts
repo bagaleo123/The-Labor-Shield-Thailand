eb6afcf0c746e275cddda02e78763ff32c48b226
bagaleo123 <bagdankor95@hotmail.com>
2026-05-10 23:33:25 +0700

    Update vite.config.ts

--- Labour shild Thailand/vite.config.ts
+++ Labour shild Thailand/vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// Стандартный конфиг для обычного React приложения
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tsconfigPaths(),
   ],
   build: {
     outDir: 'dist',
