adf4a3ebb90ff031f382a75e5ffc3518292a6516
bagaleo123 <bagdankor95@hotmail.com>
2026-05-11 00:00:30 +0700

    Update vite.config.ts

--- Labour shild Thailand/vite.config.ts
+++ Labour shild Thailand/vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
-})
+  tanstackStart: {
+    ssr: true, // Включаем серверный рендеринг для Web Service
+  },
+});
